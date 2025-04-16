a9d385c5d072ada26ae06d0f3e13179dcec8c6e0
Jaswinder Singh <jaswinder.4031@gmail.com>
2025-04-16 18:30:09 +0530

    backend complete

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -4,11 +4,11 @@ const {setCache} = require('./../middleware/cacheMiddleware');
 const api_url = process.env.API_URL;
 const api_key = process.env.API_KEY
 const getMarketData = async(req,res)=>{
-    const { vs_currency="usd", order="desc", per_page=1, page=1 } = req.query;
-    console.log(`${api_url}/markets?vs_currency=${vs_currency}&per_page=${per_page}&page=${page}`,'x-cg-demo-api-key',api_key);
+    const { vs_currency="usd", order="price_change_percentage_24h_desc", per_page=1, page=1 } = req.query;
+    console.log(`${api_url}/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}`,'x-cg-demo-api-key',api_key);
 
     const response = await axios({
-        url:`${api_url}/markets?vs_currency=${vs_currency}&per_page=${per_page}&page=${page}`,
+        url:`${api_url}/markets?vs_currency=${vs_currency}&order=${order}&per_page=${per_page}&page=${page}`,
         method:"get",
         headers:{
             'x-cg-demo-api-key':api_key
@@ -21,7 +21,7 @@ const getMarketData = async(req,res)=>{
 
 const getCoinMarketChart = async(req,res)=>{
     const { id } = req.params;
-    const { vs_currency, days } = req.query;
+    const { vs_currency="usd", days="7" } = req.query;
 
     const response = await axios({
         url: `${api_url}/${id}/market_chart?vs_currency=${vs_currency}&days=${days}`,
@@ -36,7 +36,7 @@ const getCoinMarketChart = async(req,res)=>{
 
 const getCoinById = async(req,res)=>{
     const { id } = req.params;
-    const { vs_currency } = req.query;
+    const { vs_currency="usd" } = req.query;
 
     const response = await axios({
         url: `${api_url}/${id}?vs_currency=${vs_currency}`,
